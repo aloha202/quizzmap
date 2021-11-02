dfb23fced6f577e4d6e683a592c8dc1b7e65393c
Alyosha Radziuk <alexey.radyuk@gmail.com>
2021-11-02 12:08:38 +0000

    Added parser question functionality

---
+++ public/bundles/admin.js
@@ -0,0 +1,30 @@
+$(function (){
+
+
+
+    if($('#Question_type').length) {
+        const qt_change = function () {
+            alert($(this).val());
+        }
+
+
+        /*
+        setInterval(function (){
+            console.log($('#Question_type').val());
+        }, 1000);
+
+         */
+        $('#Question_type').change(function (){
+           if($(this).val() == 1){
+               $('.admin-answers-collection').show();
+           } else {
+                $('.admin-answers-collection').hide();
+           }
+        });
+        $('#Question_type').change();
+
+    }
+
+
+
+})
