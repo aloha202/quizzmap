a270b3f0268e9233618b0ed37fac523c0e4f3af6
Alyosha Radziuk <alexey.radyuk@gmail.com>
2021-11-02 16:00:07 +0000

    ansewr controller 1

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Question;
+use App\Repository\AnswerRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController extends AbstractController
+{
+    #[Route('/question/{id}.html', name: 'question')]
+    public function show(Question $question, Request $request, AnswerRepository $answerRepository): Response
+    {
+        $answer = null;
+        if($request->isMethod('post')){
+            $answer_id = $request->request->get('answer_id');
+            if($answer_id) {
+                $answer = $answerRepository->find($answer_id);
+            }
+        }
+        $tpl = 'default';
+        switch ($question->getType())
+        {
+            case Question::CONST_TYPE_PARSER:
+                $tpl = 'parser';
+                break;
+        }
+        return $this->render('question/show_' . $tpl . '.html.twig', [
+            'question' => $question,
+            'currentAnswer' => $answer
+        ]);
+    }
+}
