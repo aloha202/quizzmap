f84df2a937670ea3e99f09ff857f44b2078dfdb5
Alyosha Radziuk <alexey.radyuk@gmail.com>
2021-11-05 18:12:10 +0000

    location parent

--- src/Controller/Admin/LocationCrudController.php
+++ src/Controller/Admin/LocationCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Location;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class LocationCrudController extends AbstractCrudController
@@ -18,10 +19,12 @@ class LocationCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
+            AssociationField::new('parent'),
             AssociationField::new('area'),
             AssociationField::new('type'),
             TextField::new('name'),
             TextField::new('position'),
+            CollectionField::new('locations')->onlyOnIndex()
         ];
     }
 
